5795292ecb98c261028b1b79e9f8ab2f943d1ead
Arif <arif.cvc13@gmail.com>
2019-06-19 10:53:19 -0500

    fixed icons and polishing css in create course page

--- MITT-Scheduler/App_Start/BundleConfig.cs
+++ MITT-Scheduler/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace MITT_Scheduler
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
+                 
         }
     }
 }
